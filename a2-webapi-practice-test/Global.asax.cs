fc79bd3579a8a7e23664a518f7c6833f35be7b95
uvarajthulasiram <uvaraj.thulasiram@gmail.com>
2016-09-17 08:46:19 -0600

    Test

--- a2-webapi-practice-test/Global.asax.cs
+++ a2-webapi-practice-test/Global.asax.cs
@@ -13,6 +13,7 @@ namespace a2_webapi_practice_test
     {
         protected void Application_Start()
         {
+            //
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
